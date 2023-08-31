8ccfc3f012b9c3b97512e0aac4828d4770c7684d
Lucas Otanez <lotanez@usc.edu>
2023-08-31 00:07:56 -0700

    mobile spacing and font weight tweaks

--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -52,9 +52,9 @@ export default function Projects() {
 
       <div id="projects-list" className="max-w-4xl sm:w-2/3 lg:w-1/2 w-80 flex flex-col items-center justify-center text-center m-auto">
 
-        <div className="project m-auto flex flex-col items-center justify-center text-center mb-20">
-          <div className="flex flex-row items-center justify-center">
-            <p className="text-4xl drop-shadow-lg">PAYWALL SENTRY</p>
+        <div className="project m-auto flex flex-col items-center justify-center text-left mb-20">
+          <div className="flex flex-row items-center justify-center mx-4">
+            <p className="text-4xl drop-shadow-lg font-bold">PAYWALL SENTRY</p>
             <Link href="https://github.com/lucasotanez/paywall-sentry" target="_blank">
               <svg className="icon-link scale-40" width="98" height="96" fill="ffffff" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#ffffff"/></svg>
             </Link>
@@ -63,7 +63,7 @@ export default function Projects() {
             <img className="z-20 duration-300" onClick={toggleInfo} src="/img/paywallsentry.png"/>
             <div className="z-10 top-0 overflow-auto translate-y-2 group-hover:rotate-3 
                 group-hover:translate-x-12 backdrop" onClick={toggleInfo}>
-              <p className="pointer-events-none mx-8 my-4 sm:text-md md:text-xl text-sm">This Google Chrome extension
+              <p className="pointer-events-none mx-8 sm:mx-12 my-4 sm:text-md md:text-xl text-sm">This Google Chrome extension
               was created in <span className="text-naples-yellow">Typescript</span> and 
               compiled with <span className="text-naples-yellow">Webpack</span>. It supports
               flagging on both hard and soft paywalls, differentiating 
@@ -71,7 +71,7 @@ export default function Projects() {
               to include subscriptions they already own to disable flags on those sites.</p>
             </div>
           </div>
-          <div className="mt-4 mb-10 text-lg flex flex-row items-center justify-center">
+          <div className="mt-4 mb-10 text-lg flex flex-row items-center justify-center mx-4">
             <p>Paywall Flagging Chrome Extension</p>
             <Link href="https://chrome.google.com/webstore/detail/paywall-sentry/felglaliecfphggeffmjejkejgdoabgh" target="_blank">
               <svg className="icon-link scale-30 -ml-6 -my-8 mr-0" xmlns="http://www.w3.org/2000/svg" width="98" height="96" viewBox="0 0 512 512"><path d="M188.8,255.93A67.2,67.2,0,1,0,256,188.75,67.38,67.38,0,0,0,188.8,255.93Z" fill="#ffffff"/><path fill="#ffffff" d="M476.75,217.79s0,0,0,.05a206.63,206.63,0,0,0-7-28.84h-.11a202.16,202.16,0,0,1,7.07,29h0a203.5,203.5,0,0,0-7.07-29H314.24c19.05,17,31.36,40.17,31.36,67.05a86.55,86.55,0,0,1-12.31,44.73L231,478.45a2.44,2.44,0,0,1,0,.27V479h0v-.26A224,224,0,0,0,256,480c6.84,0,13.61-.39,20.3-1a222.91,222.91,0,0,0,29.78-4.74C405.68,451.52,480,362.4,480,255.94A225.25,225.25,0,0,0,476.75,217.79Z"/><path fill="#ffffff" d="M256,345.5c-33.6,0-61.6-17.91-77.29-44.79L76,123.05l-.14-.24A224,224,0,0,0,207.4,474.55l0-.05,77.69-134.6A84.13,84.13,0,0,1,256,345.5Z"/><path fill="#ffffff" d="M91.29,104.57l77.35,133.25A89.19,89.19,0,0,1,256,166H461.17a246.51,246.51,0,0,0-25.78-43.94l.12.08A245.26,245.26,0,0,1,461.17,166h.17a245.91,245.91,0,0,0-25.66-44,2.63,2.63,0,0,1-.35-.26A223.93,223.93,0,0,0,91.14,104.34l.14.24Z"/></svg>
@@ -79,9 +79,9 @@ export default function Projects() {
           </div>
         </div>
 
-        <div className="project m-auto flex flex-col items-center justify-center text-center mb-20">
-          <div className="flex flex-row items-center justify-center">
-            <p className="text-4xl drop-shadow-lg">LINTERVIEW</p>
+        <div className="project m-auto flex flex-col items-center justify-center text-left mb-20">
+          <div className="flex flex-row items-center justify-center mx-4">
+            <p className="text-4xl drop-shadow-lg font-bold">LINTERVIEW</p>
             <Link href="https://github.com/linterview-project" target="_blank">
               <svg className="icon-link scale-40" width="98" height="96" fill="ffffff" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#ffffff"/></svg>
             </Link>
@@ -90,7 +90,7 @@ export default function Projects() {
             <img className="z-20 duration-300" onClick={toggleInfo} src="/img/linterview.png"/>
             <div className="z-10 top-0 translate-y-2 group-hover:rotate-3 
                 group-hover:translate-x-12 backdrop" onClick={toggleInfo}>
-              <p className="pointer-events-none mx-8 my-4 sm:text-md md:text-xl text-sm">
+              <p className="pointer-events-none mx-8 sm:mx-12 my-4 sm:text-md md:text-xl text-sm">
                 This web application allows users to practice their behavioral interview
                 answers and techniques. You can think of LinterView as <span className="italic">LeetCode for 
                 behavioral interviews.</span> The app supports user accounts and uses
@@ -101,7 +101,7 @@ export default function Projects() {
               </p>
             </div>
           </div>
-          <div className="mt-4 mb-10 text-lg flex flex-row items-center justify-center">
+          <div className="mt-4 mb-10 text-lg flex flex-row items-center justify-center mx-4">
             <p>Mock Behavioral Interview Practice Web Application</p>
             <Link href="https://www.linterview.net/" target="_blank">
               <svg className="icon-link scale-30 -ml-6 -my-8 mr-0" width="98" height="96" viewBox="0 0 174 174" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -112,9 +112,9 @@ export default function Projects() {
           </div>
         </div>
 
-        <div className="project m-auto flex flex-col items-center justify-center text-center mb-20">
-          <div className="flex flex-row items-center justify-center">
-            <p className="text-4xl drop-shadow-lg">ICARUS FREESTYLE</p>
+        <div className="project m-auto flex flex-col items-center justify-center text-left mb-20">
+          <div className="flex flex-row items-center justify-center mx-4">
+            <p className="text-4xl drop-shadow-lg font-bold">ICARUS FREESTYLE</p>
             <Link href="https://github.com/lucasotanez/icarus-freestyle" target="_blank">
               <svg className="icon-link scale-40" width="98" height="96" fill="ffffff" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#ffffff"/></svg>
             </Link>
@@ -123,7 +123,7 @@ export default function Projects() {
             <img className="z-20 duration-300" onClick={toggleInfo} src="/img/icarus.gif"/>
             <div className="z-10 top-0 translate-y-2 group-hover:rotate-3 
                 group-hover:translate-x-12 backdrop" onClick={toggleInfo}>
-              <p className="pointer-events-none mx-8 my-4 sm:text-md md:text-xl text-sm">
+              <p className="pointer-events-none mx-8 sm:mx-12 my-4 sm:text-md md:text-xl text-sm">
                 Icarus Freestyle is a side-scrolling game where you play as Icarus flying
                 freestyle through a laser maze. All of the sprites were made custom and 
                 all gameplay logic was programmed in <span className="text-naples-yellow">C++</span> from
@@ -132,36 +132,14 @@ export default function Projects() {
               </p>
             </div>
           </div>
-          <div className="mt-4 mb-10 text-lg flex flex-row items-center justify-center">
+          <div className="mt-4 mb-10 text-lg flex flex-row items-center justify-center mx-4">
             <p>C++ Game</p>
           </div>
         </div>
 
-{/*
-        <div className="project m-auto flex flex-col items-center justify-center text-center mb-20">
-          <p className="text-4xl mb-8">Icarus Freestyle</p>
-          <div className="group">
-            <img className="z-20 duration-300" onClick={toggleInfo} src="/img/linterview.png"/>
-            <div className="z-10 top-0 translate-y-2 group-hover:rotate-3 
-                group-hover:translate-x-12 backdrop" onClick={toggleInfo}>
-              <p className="pointer-events-none mx-8 my-4 sm:text-md lg:text-lg xl:text-xl text-sm">
-                Icarus Freestyle is a side-scrolling game where you play as Icarus flying
-                freestyle through a laser maze. All of the sprites were made custom and 
-                all gameplay logic was programmed in <span className="text-naples-yellow">C++</span> from
-                scratch using the <span className="text-naples-yellow">SDL</span> desktop application library.
-                Instructions to build using the Makefile can be found on Github.
-              </p>
-            </div>
-          </div>
-          <div className="mt-4 mb-10 text-lg">
-            <p>Custom C++ SDL Game</p>
-          </div>
-        </div>
-*/}
-
-        <div className="project m-auto flex flex-col items-center justify-center text-center mb-20">
-          <div className="flex flex-row items-center justify-center">
-            <p className="text-4xl drop-shadow-lg">DIGIT OCR</p>
+        <div className="project m-auto flex flex-col items-center justify-center text-left mb-20">
+          <div className="flex flex-row items-center justify-center mx-4">
+            <p className="text-4xl drop-shadow-lg font-bold">DIGIT OCR</p>
             <Link href="https://github.com/lucasotanez" target="_blank">
               <svg className="icon-link scale-40" width="98" height="96" fill="ffffff" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#ffffff"/></svg>
             </Link>
@@ -170,10 +148,10 @@ export default function Projects() {
             <img className="z-20 duration-300" onClick={toggleInfo} src="/img/ocr.png"/>
             <div className="z-10 top-0 translate-y-2 group-hover:rotate-3 
                 group-hover:translate-x-12 backdrop" onClick={toggleInfo}>
-              <p className="pointer-events-none mx-8 my-4 sm:text-md md:text-xl text-sm">
+              <p className="pointer-events-none mx-8 sm:mx-12 my-4 sm:text-md md:text-xl text-sm">
                 This is a command line utility I made for a school project. The script is
                 capable of transcribing any bitmap image containing digits. The project also
-                required me to create a "big int" class (and operations) 
+                required the creation of a "big int" class (and operations) 
                 in <span className="text-naples-yellow">C++</span> so that 
                 the number transcribed can be infinitely big. Identifying correct digits
                 involved using breadth-first search and calculating different values
@@ -182,7 +160,7 @@ export default function Projects() {
               </p>
             </div>
           </div>
-          <div className="mt-4 mb-10 text-lg">
+          <div className="mt-4 mb-10 text-lg mx-4">
             <p>Optical Character Recognition for Sequences of Numbers</p>
           </div>
         </div>
